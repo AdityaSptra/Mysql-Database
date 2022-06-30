ad43a27cee98eac96b3a543fac55af75c39e85c8
Aditya Saputra <107994392+AdityaSptra@users.noreply.github.com>
2022-06-30 14:00:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const nama = document.querySelector(".nama");
 const b = document.querySelector(".b");
+const p = document.querySelector("p");
 
 let data = ["Aditya"]:
 b.addEventListener("click",() => {
   data.push(nama.value);
 });
+p.innerHTML += data;
